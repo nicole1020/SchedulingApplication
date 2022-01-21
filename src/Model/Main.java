589215abb02e35c39321dba0a101814697a4f162
nicole <nmau@wgu.edu>
2022-01-21 02:06:06 +0000

    Day 1

--- src/Model/Main.java
+++ src/Model/Main.java
@@ -12,11 +12,13 @@ public class Main extends Application {
 public Main(){}
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = (Parent)FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/View.sample.fxml")));
-        primaryStage.setTitle(" ");
-        primaryStage.setScene(new Scene(root, 800, 1275));
+        Parent root = FXMLLoader.load(getClass().getResource("/View/MainScreen.fxml"));
+        primaryStage.setTitle("");
+        primaryStage.setScene(new Scene(root, 800,600));
         primaryStage.show();
-    }
+
+
+}
 
 
     public static void main(String[] args) {
