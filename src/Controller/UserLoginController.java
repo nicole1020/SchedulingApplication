acbd9e7b485bf06cce6655b336af095b9f473c08
nicole <nmau@wgu.edu>
2022-01-22 23:32:40 +0000

    Merge remote-tracking branch 'github/master'

--- src/Controller/UserLoginController.java
+++ src/Controller/UserLoginController.java
@@ -1,13 +1,37 @@
 package Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class UserLoginController implements Initializable {
+    public Button login;
+    public Button cancel;
+    public TextField userIDField;
+    public TextField password;
+    public Label userIDLabel;
+    public Label passwordLabel;
+    public Label userLocationLabel;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public void onLogin(ActionEvent actionEvent) {
+    }
+
+    public void onCancel(ActionEvent actionEvent) {
+    }
+
+    public void onUserIDField(ActionEvent actionEvent) {
+    }
+
+    public void onPassword(ActionEvent actionEvent) {
     }
 }
