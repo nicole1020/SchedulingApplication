068df303430d457dd3157d0abd7db296996a3fc0
nicole <nmau@wgu.edu>
2022-01-26 00:07:42 +0000

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	src/Controller/UserLoginController.java
    #	src/View/UserLoginScreen.fxml

--- src/Model/Main.java
+++ src/Model/Main.java
@@ -7,7 +7,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ 1/24/2022 I asked for help through email on user login screen. I was having trouble validating the inputs.
+ I received email back from Miss Carolyn asking if it was my inputs I was comparing or comparing themselves to strings.
+ that was my problem. I added Users class and a method to add a user to an observable array list allUsers. I also added a generateUserID method
+ because we will need to access this information later in scheduling.
 
+ Next I will work on changing the language of the login screen based on geographic location of the user.
+
+ */
 public class Main extends Application {
 
     @Override
