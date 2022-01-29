f14a2e6c433571ab4738ec398def7f6ea3a401fc
nicole1020 <71186944+nicole1020@users.noreply.github.com>
2022-01-29 15:06:54 -0800

    Update README.md

--- src/DAO/UserDAOImpl.java
+++ src/DAO/UserDAOImpl.java
@@ -8,6 +8,7 @@ import javafx.collections.ObservableList;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class UserDAOImpl {
 
